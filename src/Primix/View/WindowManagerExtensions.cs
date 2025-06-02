6e1b3078e1eb1a713cecdf370feb26c605194b51
syanmi <syanmims@gmail.com>
2025-06-03 00:00:32 +0900

    add

---
+++ src/Primix/View/WindowManagerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace Primix.View
+{
+    public static class WindowManagerExtensions
+    {
+        public static IWindow GetTopLevelWindow(this IWindowManager manager)
+        {
+            var windows = manager.GetOpenWindows();
+            return windows.FirstOrDefault(x => x.Parent == null);
+        }
+    }
+}
